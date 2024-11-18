58a5d65c6aedfcdb51d14dec7941423fe6c4bfec
Gogumi <chjw956@naver.com>
2024-11-18 23:37:36 +0900

    [Modify] 회원 가입 및 로그인 기능 수정

--- backend/MyDiary/accounts/urls.py
+++ backend/MyDiary/accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', include('dj_rest_auth.urls')),
+    path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
 ]
